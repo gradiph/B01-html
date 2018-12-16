b0f372aeb129d6b716f05b4a6e1729205ed03310
Baruna <barunamarines@gmail.com>
2018-12-17 00:53:17 +0700

    Revisi Tugas HTML 5 NEW

--- application/views/halaman2.php
+++ application/views/halaman2.php
@@ -45,34 +45,29 @@
 				<form method="post" action="halaman2.php" id="form-daftar">
 					<p>
 						<label for="input_username">Nama:</label>
-						<input type="text" id="input_username" placeholder=""/>
+						<input type="text" id="input_username" name="input_username" />
 					</p>
 					<p>
 						<label for="input_email">Email:</label>
-						<input type="Email" id="input_email" placeholder=""/>
+						<input type="Email" id="input_email" name="input_email" />
 					</p>
 					<p>
 						<label for="input_tanggal_lahir">Tgl. lahir:</label>
-						<input date="date" id="input_tanggal_lahir" placeholder="YYYY/MM/DD"/>
+						<input date="date" id="input_tanggal_lahir" name="input_tanggal_lahir" placeholder="YYYY/MM/DD"/>
 					</p>
-					
 					<label for="Jenis_kelamin">Jenis Kelamin:</label>
 					<input type="radio" name="Jenis_kelamin" id="Jenis_kelamin" value="Laki_laki"/>Laki-laki
 					<input type="radio" name="Jenis_kelamin" id="Jenis_kelamin" value="Perempuan"/>Perempuan
 					<input type="radio" name="Jenis_kelamin" id="Jenis_kelamin" value="Tidak_Dijelaskan" />Tidak dijelaskan
 					<p>
 						<label for="hobi[]">Hobi:</label>
-						<input type="checkbox" name="hobi[]" value="Berkendara"/>Berkendara
-						<input type="checkbox" name="hobi[]" value="Bermain-Musik"/>Bermain Musik
-						<input type="checkbox" name="hobi[]" value="Belajar"/>Belajar
+						<input type="checkbox" name="hobi[]" id="hobi[]" value="Berkendara"/>Berkendara
+						<input type="checkbox" name="hobi[]" id="hobi[]" value="Bermain-Musik"/>Bermain Musik
+						<input type="checkbox" name="hobi[]" id="hobi[]" value="Belajar"/>Belajar
 						<br>
 						<br>
-						<td>
-							<input type="submit"  id="Simpan" value="Simpan">
-						</td>
-						<td>
-							<input type="Reset" id="Reset" value="Reset">
-						</td>
+						<td><input type="submit"  id="Simpan" value="Simpan" name="Simpan"></td>
+						<td><input type="Reset" id="Reset" value="Reset" name="Reset"></td>
 					</p>
 				</section>
 				<hr width="100%">
@@ -82,8 +77,8 @@
 					<a name="menujutabel">
 						<h3>TABEL</h3>
 					</a>
-					<thead>
-						<table border="1">
+					<table border="1">
+						<thead>
 							<tr>
 								<th style="text-align: center;" scope="col">NO</th>
 								<th style="text-align: center;" scope="col">NAMA</th>
@@ -92,37 +87,38 @@
 								<th style="text-align: center;" scope="col">DESKRIPSI 3</th>
 							</tr>
 						</thead>
-						<tr>
-							<th style="text-align: center;" scope="row">1</th>
-							<td style="text-align: center;">Baruna</td>
-							<td style="text-align: center;">ABC</td>
-							<td style="text-align: center;">DEF</td>
-							<td style="text-align: center;">GHI</td>
-						</tr>
-						<tr>
-							<th style="text-align: center;" scope="row">2</th>
-							<td style="text-align: center;">Gradi</td>
-							<td style="text-align: center;">JKL</td>
-							<td colspan="2" style="text-align: center;">MNO</td>
-							
-						</tr>
-						<tr>
-							<th style="text-align: center;" scope="row">3</th>
-							<td style="text-align: center;">Tita</td>
-							<td rowspan="2" style="text-align: center;">PQR</td>
-							<td style="text-align: center;">STU</td>
-							<td style="text-align: center;">VWX</td>
-						</tr>
-						<tr>
-							<th style="text-align: center;" scope="row">4</th>
-							<td style="text-align: center;">Yohana</td>
-							<td style="text-align: center;">YZA</td>
-							<td style="text-align: center;">BCD</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th style="text-align: center;" scope="row">1</th>
+								<td style="text-align: center;">Baruna</td>
+								<td style="text-align: center;">ABC</td>
+								<td style="text-align: center;">DEF</td>
+								<td style="text-align: center;">GHI</td>
+							</tr>
+							<tr>
+								<th style="text-align: center;" scope="row">2</th>
+								<td style="text-align: center;">Gradi</td>
+								<td style="text-align: center;">JKL</td>
+								<td colspan="2" style="text-align: center;">MNO</td>
+							</tr>
+							<tr>
+								<th style="text-align: center;" scope="row">3</th>
+								<td style="text-align: center;">Tita</td>
+								<td rowspan="2" style="text-align: center;">PQR</td>
+								<td style="text-align: center;">STU</td>
+								<td style="text-align: center;">VWX</td>
+							</tr>
+							<tr>
+								<th style="text-align: center;" scope="row">4</th>
+								<td style="text-align: center;">Yohana</td>
+								<td style="text-align: center;">YZA</td>
+								<td style="text-align: center;">BCD</td>
+							</tr>
+						</tbody>
 					</table>
-					<hr width="100%">
 				</section>
 			</main>
+			<hr width="100%">
 			
 			<!-- Isi Footer -->
 			<footer>
